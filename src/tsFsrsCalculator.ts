e6e937d54cf47521124d2a297b00c866dda59885
ishiko <ishiko732@gmail.com>
2024-09-07 02:28:14 +0800

    switch of enable/disable short-term schedule

--- src/tsFsrsCalculator.ts
+++ src/tsFsrsCalculator.ts
@@ -4,10 +4,12 @@ import { Card, type IFsrsCalculator } from "./IFsrsCalculator";
 export class TsFsrsCalculator implements IFsrsCalculator {
     readonly w: number[];
     readonly desiredR: number;
+    readonly enableShortTerm: boolean;
 
-    public constructor(w: number[], m: number[]) {
+    public constructor(w: number[], m: number[], enable: boolean) {
         this.w = w;
         this.desiredR = m[0];
+        this.enableShortTerm = enable;
     }
 
     calcDisplayDifficulty(d: number) {
@@ -17,7 +19,7 @@ export class TsFsrsCalculator implements IFsrsCalculator {
     public steps(reviews: number[]): Card[] {
         let fsrs_card = createEmptyCard(new Date());
 
-        let card = new Card(true, 0.0, 0.0, 0.0, 0.0, 0.0, 0);
+        let card = new Card(0, 0.0, 0.0, 0.0, 0.0, 0.0, 0);
         const list = [];
 
         const f = fsrs(generatorParameters({
@@ -39,7 +41,7 @@ export class TsFsrsCalculator implements IFsrsCalculator {
                 fsrs_card.due = new Date(date.getTime() + interval * 24 * 60 * 60 * 1000);
             }
 
-            list.push(new Card(false, fsrs_card.difficulty, displayDifficulty, fsrs_card.stability, interval, cumulativeInterval, review));
+            list.push(new Card(0, fsrs_card.difficulty, displayDifficulty, fsrs_card.stability, interval, cumulativeInterval, review));
         }
 
         return list;

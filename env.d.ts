f478de2a108b11d9232bea8590fc1bdac6d498a8
xiety <xiety666@gmail.com>
2025-04-04 15:33:42 +0300

    update dependencies

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+import 'chart.js';
+import { ZoomPluginOptions } from 'chartjs-plugin-zoom';
+import { DataLabelsPluginOptions } from 'chartjs-plugin-datalabels';
+
+declare module 'chart.js' {
+    interface PluginOptionsByType<TType extends ChartType> {
+        zoom?: ZoomPluginOptions;
+        datalabels?: DataLabelsPluginOptions;
+    }
+}
